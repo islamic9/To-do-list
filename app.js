9d8788abfbda9caa37ac9f582251ba47663822a6
islamic9 <gitbushprofile01@gmail.com>
2023-03-04 19:06:04 +0500

    language changing

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ $form.addEventListener("submit", e => {
     $taskMainElement.appendChild($btnsWrapperElement);
     $btnsWrapperElement.className = "task-item__btn-wrapper";
     $btnsWrapperElement.innerHTML = `
-    <button class="complete"> <i class="fas fa-circle-check"> </i> <br> Complete </button>
-    <button class="edit"> <i class="fas fa-edit"> </i> <br> Edit </button>
+    <button class="complete"> <i class="fas fa-circle-check"> </i> <br> Bajarildi! </button>
+    <button class="edit"> <i class="fas fa-edit"> </i> <br> Tahrirlash </button>
     <button> <i class="fas fa-clock"> </i> <br> ${time.getHours().toString().padStart(2, "0")} : ${time.getMinutes().toString().padStart(2, "0")} : ${time.getSeconds().toString().padStart(2, "0")} </button>
-    <button class="delete"> <i class="fas fa-trash"> </i> <br> Delete </button>`
+    <button class="delete"> <i class="fas fa-trash"> </i> <br> O'chirish </button>`
     $taskMainElement.className = 'task-item'
     $taskContainer.appendChild($taskMainElement); 
     $todoInput.value = null;
@@ -29,7 +29,9 @@ $taskContainer.addEventListener('click', (e) => {
         e.target.parentElement.previousSibling.classList.toggle("completed")
     }
     else if(e.target.className.includes("delete")){
-        const isAgreedToDelete = confirm('are you sure to delete this item')
+        const isAgreedToDelete = document.createElement("div");
+        isAgreedToDelete.className = "prompt"
+        console.log('isAgreedToDelete');
         if(isAgreedToDelete){
             e.target.parentElement.parentElement.classList.add("remove-item")
             setTimeout(() => {
@@ -41,13 +43,13 @@ $taskContainer.addEventListener('click', (e) => {
         if(e.target.parentElement.previousSibling.hasAttribute("contenteditable")){
           e.target.parentElement.previousSibling.removeAttribute("contenteditable") 
           e.target.parentElement.previousSibling.classList.remove("changing")
-          e.target.innerHTML ='<i class="fas fa-edit"> </i> <br> Edit'
+          e.target.innerHTML ='<i class="fas fa-edit"> </i> <br> Tahrirlash'
           e.target.style.background = "gold"
         }    
         else{
           e.target.parentElement.previousSibling.setAttribute("contenteditable", true) 
           e.target.parentElement.previousSibling.classList.add("changing")
-          e.target.innerHTML ='<i class="fa-solid fa-check-double"> </i> <br> Done'
+          e.target.innerHTML ='<i class="fa-solid fa-check-double"> </i> <br> Tayyor'
           e.target.style.background = "purple"
         }
     }
